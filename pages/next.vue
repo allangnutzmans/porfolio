bd95dc8e3de5344032fe9a26e3833f241d60f35b
Allan Gnutzmans <allan.sgnutzman@gmail.com>
2025-07-09 23:29:41 -0300

    Scaffold to Web3 projects

--- pages/next.vue
+++ pages/next.vue
@@ -2,5 +2,6 @@
   <div>
     <h1>Next</h1>
     <p>Página dedicada à tecnologia Next.js.</p>
+    <p>Chase - AntDesign, prisma</p>
   </div>
-</template> 
+</template>
--- pages/web3/projects/index.vue
+++ pages/web3/projects/index.vue
@@ -2,59 +2,85 @@
 </script>
 
 <template>
-  <div class="content-section">
-    <div class="content-section-title">Apps in your plan</div>
-    <div class="apps-card">
-      <AppCard
-        title="Premiere Pro"
-        subtext="Edit, master and create fully proffesional videos"
-      >
-        <template #icon>
-          <IconsPr />
-        </template>
-        <template #footer>
-          <button
-            class="content-button status-button"
-            @click="mainStore.togglePopup()"
-          >
-            Update
-          </button>
-        </template>
-      </AppCard>
+  <div class="content-wrapper">
+    <div class="content-section">
+      <div class="content-section-title">FOUNDRY / dApps</div>
+      <span>Aqui vai ser adicionado os cards dos projetos, que vão ser links para eles.</span>
+      <span>O botão vai abrir uma prévia de cada num .</span>
+      <span>Ícones das techs usadas (no card).</span>
+      <div class="apps-card">
+        <AppCard
+          title="Fund me"
+          subtext="Edit, master and create fully proffesional videos"
+        >
+          <template #icon>
+            <IconsPr />
+          </template>
+          <template #footer>
+            <button
+              class="content-button status-button"
+              @click="mainStore.togglePopup()"
+            >
+              Update
+            </button>
+          </template>
+        </AppCard>
 
-      <AppCard
-        title="InDesign"
-        subtext="Design and publish great projects & mockups"
-      >
-        <template #icon>
-          <IconsId />
-        </template>
-        <template #footer>
-          <button
-            class="content-button status-button"
-            @click="mainStore.togglePopup()"
-          >
-            Update
-          </button>
-        </template>
-      </AppCard>
+        <AppCard
+          title="Lottery"
+          subtext="Design and publish great projects & mockups"
+        >
+          <template #icon>
+            <IconsId />
+          </template>
+          <template #footer>
+            <button
+              class="content-button status-button"
+              @click="mainStore.togglePopup()"
+            >
+              Update
+            </button>
+          </template>
+        </AppCard>
 
-      <AppCard
-        title="After Effects"
-        subtext="Industry Standart motion graphics & visual effects"
-      >
-        <template #icon>
-          <IconsAe />
-        </template>
-        <template #footer>
-          <button
-            class="content-button status-button"
-            @click="mainStore.togglePopup()"
-          >
-            Update
-          </button>
-        </template>
-      </AppCard>
+        <AppCard
+          title="Nft"
+          subtext="Industry Standart motion graphics & visual effects"
+        >
+          <template #icon>
+            <IconsAe />
+          </template>
+          <template #footer>
+            <button
+              class="content-button status-button"
+              @click="mainStore.togglePopup()"
+            >
+              Update
+            </button>
+          </template>
+        </AppCard>
+
+        <AppCard
+          title="Elections Dapp"
+          subtext="Industry Standart motion graphics & visual effects"
+        >
+          <template #icon>
+            <IconsAe />
+          </template>
+          <template #footer>
+            <button
+              class="content-button status-button"
+              @click="mainStore.togglePopup()"
+            >
+              Update
+            </button>
+          </template>
+        </AppCard>
+      </div>
+    </div>
+    <div class="content-section">
+      <div class="content-section-title">Code</div>
+      GITHUB REPO
     </div>
   </div>
 </template>
